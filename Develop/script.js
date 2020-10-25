78dda9c3af40ff51be954b3fa28d676c2a6b0950
Maria Claudia Marturet <claudiamarturet@Marias-MBP.attlocal.net>
2020-10-25 15:34:38 -0700

    floor length

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,31 @@
 // Assignment code here
-const password = document.get.ElementById
-('password')
-const generate = document.getElementById
-<'generate'
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercase = "abcedefghijklmnopqrstuvwxyz"
+var numbers = "0123456789"
+var symbols = "!@#$%^&*"
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
+console.log(uppercase, lowercase, numbers, symbols);
 
 // Write password to the #password input
 function writePassword() {
+  var password = 0 <= 128
+  for (var i = 0; 1 <= 128; i++) {
+    var randomNumber = Math.floor(Math.random ()
+    * 128 +1);
+    console.log(randomNumber);
+  }
+  function generateNumber(min,max) {
+    return Math.floor(Math.random()(max - min +1))+min;
+  }
+
+  // set password 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 'abcedefghijklmnopqrstuvwxyz0123456789@#$';
-
-  // Loop 
-  for (i = 1; 1<= 8; i++) {
-    var char = Math.floor (Math.random ()
-    * str.length +1);
-
-    generatePassword += str.charAt(char)
-  }
-  return generatePassword
-
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
